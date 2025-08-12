44e155a371b5ed7fe18e84b2a9ddcafc10e675c0
Neel Gonuguntla <neel.gonuguntla@gmail.com>
2025-08-12 10:30:44 +0530

    Create act5.java

---
+++ CS2053 -- JAVA/Lab1/act5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class prime
+{
+    public static void main(String [] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num=sc.nextInt();
+        System.out.println();
+        int c=0;
+        for(int i=1;i<=num;i++)
+        {
+            if(num%i==0)
+            {
+                c=c+1;
+            }
+        }
+        if(c==2)
+        {
+            System.out.println(num+" is a prime number");
+        }
+        else
+        {
+            System.out.println(num+" is not a prime number");
+        }
+        
+    }
+}
