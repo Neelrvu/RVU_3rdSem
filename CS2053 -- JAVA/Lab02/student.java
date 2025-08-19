9c1b46238802a9dc76be4a719e50f9edd6b3692d
Neelrvu <neelchowdharygonuguntlabca24@rvu.edu.in>
2025-08-19 10:33:03 +0530

    Create student.java

---
+++ CS2053 -- JAVA/Lab02/student.java
@@ -0,0 +1,19 @@
+package javalab;
+
+public class student {
+	String name;
+	int age;
+	
+	void displayInfo()
+	{
+		System.out.println("Name : "+name+", Age : "+age);
+	}
+	
+	public static void main(String [] args)
+	{
+		student s1=new student();
+		s1.name="John";
+		s1.age=20;
+		s1.displayInfo();
+	}
+}
