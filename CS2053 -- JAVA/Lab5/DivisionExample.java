2fb5f34e4b01066a9595f4c6d080ab8330dd6bf1
Neelrvu <neelchowdharygonuguntlabca24@rvu.edu.in>
2025-09-16 10:17:09 +0530

    Create DivisionExample.java

---
+++ CS2053 -- JAVA/Lab5/DivisionExample.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DivisionExample {
+	public static void main(String [] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter numerator: ");
+		int numerator=sc.nextInt();
+		
+		System.out.println("Enter denominator: ");
+		int denominator=sc.nextInt();
+		
+		try
+		{
+			int result=numerator/denominator;
+			System.out.println("Result : "+result);
+		
+		}catch(ArithmeticException e) {
+			System.out.println("Error:Cannot divide by zero!");
+		}
+		
+		System.out.println("Program Continues...");
+		sc.close();
+	}
+}
